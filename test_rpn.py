6935f080f83ee0cde6ee8f99b0efa8276c19b014
Spencer <spencersortman1@gmail.com>
2019-10-30 15:23:53 -0400

    Add test for pow

--- test_rpn.py
+++ test_rpn.py
@@ -5,3 +5,6 @@ class TestBasics(unittest.TestCase):
 	def test_add(self):
 		result = rpn.calculate('1 1 + ')
 		self.assertEqual(2, result)
+	def test_pow(self):
+		result = rpn.calculate('2 3 ^ ')
+		self.assertEqual(8, result)
