6dfa2799f43a4500f9c619f40ae0bd6fd55b57fa
Spencer <spencersortman1@gmail.com>
2019-10-30 16:29:16 -0400

    Implements functionality for pow function

--- rpn.py
+++ rpn.py
@@ -9,6 +9,11 @@ def calculate(arg):
 			arg1 = stack.pop()
 			result = arg1 + arg2
 			stack.append(result)
+		elif token == '^':
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = arg1 ** arg2
+			stack.append(result)	
 		else:
 			stack.append(int(token))
 	return stack.pop()
