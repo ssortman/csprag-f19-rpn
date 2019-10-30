41091ebcc1e37cd797cc9c5422a307f194e25cd9
Spencer <spencersortman1@gmail.com>
2019-10-30 13:49:36 -0400

    Test for + passes

--- rpn.py
+++ rpn.py
@@ -1,12 +1,23 @@
 #/usr/bin/env python3
 
 
-def calculate(string):
-	pass
-
+def calculate(arg):
+	stack = list()
+	for token in arg.split(): 		
+		if token == '+':
+			arg2 = stack.pop()
+			arg1 = stack.pop()
+			result = arg1 + arg2
+			stack.append(result)
+		else:
+			stack.append(int(token))
+	return stack.pop()
+			
 def main():
 	while True:
 		calculate(input('rpn calc> '))
 
 if __name__ == '__main__':
 	main()
+
+
